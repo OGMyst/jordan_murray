750f5f93922543ad2c205b94169af42ac0a5d809
Hugh Padmore <62993643+OGMyst@users.noreply.github.com>
2021-10-30 14:42:23 +0100

    Added nav menu and background picture

--- Jordan_Murray/settings.py
+++ Jordan_Murray/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
