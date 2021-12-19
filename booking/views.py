7ddcadb089e58355ba2ef8a09af86a09813b7ee1
Hugh Padmore <62993643+OGMyst@users.noreply.github.com>
2021-12-19 18:06:52 +0000

    Basic form logic working

--- booking/views.py
+++ booking/views.py
@@ -1,19 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .forms import BookingForm, TeachingForm
 
 
 def booking(request):
     """ A view to return the booking page """
+    booking_form = BookingForm()
+    teaching_form = TeachingForm()
+
+    form_lookup = {
+        'booking': booking_form,
+        'teaching': teaching_form,
+    }
     if request.method == 'POST':
-        print('HI')
+        # Decide 2nd step in form by using service
+        if 'one' in request.POST:
+            service = request.POST['service'].lower()
+            context = {
+                'form': form_lookup[service],
+                'step': 'two',
+            }
+        if 'two' in request.POST:
+            return redirect(reverse('home'))
     else:
-        booking_form = BookingForm()
-        teaching_form = TeachingForm()
+        context = {
+            'form': booking_form,
+            'step': 'one',
+        }
 
     template = 'booking/booking.html'
-    context = {
-        'booking_form': booking_form,
-        'teaching_form': teaching_form,
-    }
 
     return render(request, template, context)
