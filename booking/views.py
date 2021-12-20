c5255f1ca4cddbdd910c85eb50cdb6101b501693
Hugh Padmore <62993643+OGMyst@users.noreply.github.com>
2021-12-20 14:05:33 +0000

    Changed booking form logic. Added styling to form

--- booking/views.py
+++ booking/views.py
@@ -3,30 +3,15 @@ from .forms import BookingForm, TeachingForm
 
 
 def booking(request):
-    """ A view to return the booking page """
+    """A view to return the booking page"""
     booking_form = BookingForm()
     teaching_form = TeachingForm()
 
-    form_lookup = {
-        'booking': booking_form,
-        'teaching': teaching_form,
+    context = {
+        "booking_form": booking_form,
+        "ßteaching_form": teaching_form,
     }
-    if request.method == 'POST':
-        # Decide 2nd step in form by using service
-        if 'one' in request.POST:
-            service = request.POST['service'].lower()
-            context = {
-                'form': form_lookup[service],
-                'step': 'two',
-            }
-        if 'two' in request.POST:
-            return redirect(reverse('home'))
-    else:
-        context = {
-            'form': booking_form,
-            'step': 'one',
-        }
 
-    template = 'booking/booking.html'
+    template = "booking/booking.html"
 
     return render(request, template, context)
